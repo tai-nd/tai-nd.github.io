9669b25f508eb5e3a55efb0d2ee669fcb77cd15d
tai-nd <tai-nd@users.noreply.github.com>
2025-01-01 14:50:26 +0000

    deploy: 4aa0c30adacf1076d7ef518ad96dde670d0d81c0

---
+++ _next/static/chunks/pages/_app-ecdb1f13024445c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{5401:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(6607)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(5401),n(5893))),_N_E=_.O()}]);
